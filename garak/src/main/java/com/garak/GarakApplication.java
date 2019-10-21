d19d4519f66168970889f792c6e2ae76bd29f131
nuhwang <nuhwang@digitalworks.co.kr>
2019-10-21 16:02:39 +0900

    commit

---
+++ garak/src/main/java/com/garak/GarakApplication.java
@@ -0,0 +1,13 @@
+package com.garak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarakApplication.class, args);
+	}
+
+}
